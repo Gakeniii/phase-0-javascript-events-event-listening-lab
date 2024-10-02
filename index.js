6e04fb01b292dea9c6a68e51232569a2fa928766
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-10-02 12:24:35 +0300

    Events listening

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+addingEventListener()
+
+// CALLING THE FUNCTION NAME AS AN ARGUMENT INSTEAD OF THE FUNCTION ITSELF.
+// function addEventListener(){
+//     const input = document.getElementById('button');
+
+//     function alertMe(){
+//         alert('I have been clicked!')
+//     }
+
+//     input.addEventListener('click', alertMe)
+// }
+// addEventListener()
